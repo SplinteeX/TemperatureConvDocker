36b44a8fc69fc1b31e7810b52e823eb9853d5563
SplinteeX <Jepsteri04@gmail.com>
2024-09-27 12:36:59 +0300

    ..

---
+++ src/main/java/TempConverter.java
@@ -0,0 +1,8 @@
+public class TempConverter {
+    public static void main(String[] args) {
+        System.out.println("Hello, docker!");
+        System.out.println(TemperatureConverter.fahrenheitToCelsius(212));
+        System.out.println(TemperatureConverter.celsiusToFahrenheit(0));
+
+    }
+}
